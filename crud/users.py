cec207ceddca8c5aa4c6ace8c35a1beebda09385
Neftali Arturo Hdz V. <neftali@arturohernandez@gmail.com>
2025-01-29 14:55:43 -0600

    add get_user function to retrieve users from the database with pagination

---
+++ crud/users.py
@@ -0,0 +1,6 @@
+import models.users 
+import schemas.users
+from sqlalchemy.orm import Session
+
+def get_user(db: Session, skip : int = 0, limit : int = 0):
+    return db.query(models.users.User).offset(skip).limit(limit).all()
