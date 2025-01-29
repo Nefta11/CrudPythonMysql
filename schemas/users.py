b3d4788bac70544e9c1ca4b6a80ee3d04910a42f
Neftali Arturo Hdz V. <neftali@arturohernandez@gmail.com>
2025-01-29 14:47:06 -0600

    add user base schema with additional fields for user information

--- schemas/users.py
+++ schemas/users.py
@@ -1,3 +1,16 @@
 from typing import List, Optional
 from pydantic import BaseModel
 from datetime import datetime
+
+class UserBase(BaseModel):
+    nombre: str
+    primerApellido: str
+    segundoApellido: str
+    TipoUsuario: str
+    nombreUsuario: str
+    correoElectronico: str
+    contrasena: str
+    numeroTelefono: str
+    estatus: str
+    fechaRegistro: datetime
+    fechaActualizacion: datetime
