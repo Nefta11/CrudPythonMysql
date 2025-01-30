f716f6c2cac15d4c2b57d5c03b0c6038d2d1a621
Nefta11 <neftaliarturohernandez@gmail.com>
2025-01-30 11:41:51 -0600

    agregar función para obtener un usuario por ID y ajustar límite predeterminado en get_user

--- crud/users.py
+++ crud/users.py
@@ -1,6 +1,11 @@
-import models.users 
+import models.users
 import schemas.users
 from sqlalchemy.orm import Session
 
-def get_user(db: Session, skip: int = 0, limit: int = 10):
+
+def get_user(db: Session, skip: int = 0, limit: int = 0):
     return db.query(models.users.User).offset(skip).limit(limit).all()
+
+
+def get_user(db: Session, id: int):
+    return db.query(models.users.User).filter(models.users.User, id=id).first()
--- routes/users.py
+++ routes/users.py
@@ -10,14 +10,24 @@ user = APIRouter()
 
 models.users.Base.metadata.create_all(bind=config.db.engine)
 
+
 def get_db():
     db = config.db.SessionLocal()
     try:
         yield db
     finally:
         db.close()
-        
+
+
 @user.get("/users/", response_model=List[schemas.users.User], tags=["Usuarios"])
 async def read_users(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
     db_users = crud.users.get_user(db, skip=skip, limit=limit)
     return db_users
+
+
+@user.get("/user/{id}", response_model=schemas.users.User, tags=["Usuarios"])
+async def read_user(id: int, db: Session = Depends(get_db)):
+    db_user = crud.users.get_user(db=db, id=id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
