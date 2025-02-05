93fb2e5a77a8b712a5cf9ae21835c6151d08caed
Nefta11 <neftaliarturohernandez@gmail.com>
2025-02-05 14:55:53 -0600

    agregar operaciones CRUD para usuarios: crear, leer, actualizar y eliminar

--- routes/users.py
+++ routes/users.py
@@ -25,9 +25,37 @@ async def read_users(skip: int = 0, limit: int = 10, db: Session = Depends(get_d
     return db_users
 
 
-@user.get("/user/{id}", response_model=schemas.users.User, tags=["Usuarios"])
+@user.post("/user/{id}", response_model=schemas.users.User, tags=["Usuarios"])
 async def read_user(id: int, db: Session = Depends(get_db)):
     db_user = crud.users.get_user(db=db, id=id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
+
+
+@user.post("/users/", response_model=schemas.users.User, tags=["Usuarios"])
+def create_user(user: schemas.users.UserCreate, db: Session = Depends(get_db)):
+    db_user = crud.users.get_user_by__usuario(db, usuario=user.nombreUsuario)
+    if db_user:
+        raise HTTPException(
+            status_code=400, detail="Usuario existente, intente nuevamente"
+        )
+    return crud.users.create_user(db=db, user=user)
+
+
+@user.put("/users/{id}", response_model=schemas.users.User, tags=["Usuarios"])
+async def update_user(
+    id: int, user: schemas.users.UserUpdate, db: Session = Depends(get_db)
+):
+    db_user = crud.users.update_user(db=db, id=id, user=user)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
+
+
+@user.delete("/users/{id}", response_model=schemas.users.User, tags=["Usuarios"])
+async def delete_user(id: int, db: Session = Depends(get_db)):
+    db_user = crud.users.delete_user(db=db, id=id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return db_user
