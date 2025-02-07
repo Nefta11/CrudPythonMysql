ad8aefab11316ae98d3fe86bb63bd3de5ec374cf
Nefta11 <neftaliarturohernandez@gmail.com>
2025-02-06 22:11:22 -0600

    agregar docstring para la configuración de la base de datos en db.py

--- config/db.py
+++ config/db.py
@@ -1,3 +1,7 @@
+"""
+Configuración de la base de datos para el proyecto CrudPythonMysql.
+"""
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
