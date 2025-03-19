9f793329e303a612ec0527e148f6c75a3b3ea2d0
Nefta11 <neftaliarturohernandez@gmail.com>
2025-03-19 14:35:46 -0600

    cargar variables de entorno para la configuración de la base de datos

--- config/db.py
+++ config/db.py
@@ -2,11 +2,18 @@
 Configuración de la base de datos para el proyecto CrudPythonMysql.
 """
 
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:1234567@localhost:3306/pruebadb"
+# Cargar las variables de entorno desde el archivo .env
+load_dotenv()
+
+# Obtener la URL de la base de datos desde las variables de entorno
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
